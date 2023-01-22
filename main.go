c28d4b581e52ff6838ec49af74c43cae04d30d2b
jesse-rb <jesse.reynekebarnard@gmail.com>
2023-01-22 21:11:20 +1300

    documentation

--- main.go
+++ main.go
@@ -9,7 +9,5 @@ func init() {
 }
 
 func main() {
-    logger.LogError("main", "something horrible happened.", []int{1,2,3})
-    logger.LogInfo("main", "Something worth noting happened.", []int{1,2,3})
 
 }
